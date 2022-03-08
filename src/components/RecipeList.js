5d2cb24e10cd92518fcfc4b3060ec7898af7a822
James <jperson1@umbc.edu>
2022-03-08 16:00:13 -0500

    Simple ingredient functionality

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -3,14 +3,32 @@ import { recipes } from "../data/core.js"
 
 export default function RecipeList() {
     return (
-        <div class="flex flex-wrap w-1/4 place-content-center">
-            {
-                recipes.map((recipe) => (
-                    <div key={recipe.name} class="p-1 w-full border-2">
-                        {recipe.name}
+        <div className="float-left flow-root w-1/3 p-4">
+            {recipes.map((recipe) => (
+                <div>
+                    <div key={recipe.name} className="w-full border-2 border-b-0 p-2">
+                        <p className="font-medium">{parseName(recipe.name)}</p>
                     </div>
-                ))
-            }
+
+                    <div key={recipe.name} className="flex-none border-2 border-t-0 p-2">
+                        <p className="font-medium">{ingredientList(recipe)}</p>
+                    </div>
+                </div>
+            ))}
         </div>
     );
+}
+
+// ex. "speed-module-2" to "Speed Module 2"
+function parseName(name){
+    return name.split("-").join(" ");
+}
+
+function ingredientList(recipe) {
+    var arr = [];
+    for (var item in recipe.ingredients) {
+        arr.push(recipe.ingredients[item] + " " + item);
+    }
+    var result = arr.join(", ");
+    return result;
 }
