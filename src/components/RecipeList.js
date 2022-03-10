fce937e88a1070f17559325cc04f3918dfcb8a42
James <jperson1@umbc.edu>
2022-03-10 11:31:43 -0500

    Grid, organization

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -17,16 +17,22 @@ export default function RecipeList() {
         return <div>Loading! Please wait, or try reloading the page.</div>
     } else {
         return (
-            <div className="flex">
-                <div className="overflow-y-scroll h-screen w-1/3 p-4">
+            <div className="grid grid-rows-3 grid-cols-3">
+                <div className="row-span-3 h-screen
+                                overflow-y-scroll overflow-x-hidden">
                     {recipes.map((recipe) => (
                         <button key={recipe.name} className="w-full border-2 border-b-0 p-2" onClick={() => setSelected(recipe.name)}>
                             <p className="pl-2 flex justify-start font-medium">{parseName(recipe.name)}</p>
                         </button>
                     ))}
                 </div>
-                <div className="overflow-y-scroll w-2/3 p-4">
+                <div className="col-span-2 p-4 border-2
+                                overflow-y-scroll overflow-x-hidden">
                     <Info name={selected} />
+                </div>
+                <div className="row-span-2 col-span-2 p-4 border-2
+                                overflow-y-scroll overflow-x-hidden">
+                    heblo
                 </div>
             </div>
 
