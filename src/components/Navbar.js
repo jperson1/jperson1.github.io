196995b11efc894c943c7126ac8960a53831d44e
James <jperson1@umbc.edu>
2022-04-09 12:52:15 -0400

    Formatting update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,11 +10,13 @@ export default function Navbar() {
     return (
         <header className="bg-gray-800 shadow-md
                         md:sticky top-0 z-10 text-white">
-            <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+            <div className="container p-5 space-y-1
+                            flex flex-wrap flex-col md:flex-row 
+                            items-center">
 
-                <Link to="/"><HomeIcon className="text-purple-400 hover:text-purple-500 w-10 inline-block" /></Link>
+                <Link to="/"><HomeIcon className="text-purple-400 hover:text-purple-500 w-10" /></Link>
 
-                <nav className="md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400 
+                <nav className="pt-2 md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400 
                             hover:font-bold">
                     <Link to="/about">About This Site</Link>
                 </nav>
@@ -30,7 +32,7 @@ export default function Navbar() {
                 </nav>
 
                 <nav className="md:ml-8 md:absolute md:right-8">
-                    <a className="pr-4 hover:font-bold" href="https://github.com/jperson1">GitHub</a>
+                    <a className="pr-8 hover:font-bold" href="https://github.com/jperson1">GitHub</a>
                     <a className="hover:font-bold" href="https://www.linkedin.com/in/james-person-85aa1422b/">LinkedIn</a>
                 </nav>
 
