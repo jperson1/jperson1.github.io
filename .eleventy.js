12ab34e84e7e4666f06a5cdd66aa47275953de44
James Person <jperson1@umbc.edu>
2025-04-27 16:16:35 -0400

    Tailwindcss v4 and a test homepage

--- .eleventy.js
+++ .eleventy.js
@@ -1,14 +1,52 @@
+import fs from 'fs';
+import path from 'path';
+
+import cssnano from 'cssnano';
 import eleventyNavigationPlugin from "@11ty/eleventy-navigation";
+import postcss from 'postcss';
+import tailwindcss from '@tailwindcss/postcss';
 
 export default (config) => {
+    //compile tailwind before eleventy processes the files
+    config.on('eleventy.before', async () => {
+        const tailwindInputPath = path.resolve('./src/assets/css/index.css');
+
+        const tailwindOutputPath = './_site/assets/css/index.css';
+
+        const cssContent = fs.readFileSync(tailwindInputPath, 'utf8');
+
+        const outputDir = path.dirname(tailwindOutputPath);
+        if (!fs.existsSync(outputDir)) {
+        fs.mkdirSync(outputDir, { recursive: true });
+        }
+
+        const result = await processor.process(cssContent, {
+        from: tailwindInputPath,
+        to: tailwindOutputPath,
+        });
+
+        fs.writeFileSync(tailwindOutputPath, result.css);
+    });
+
+    const processor = postcss([
+        //compile tailwind
+        tailwindcss(),
+
+        //minify tailwind css
+        cssnano({
+        preset: 'default',
+        }),
+    ]);
+
     config.addPlugin(eleventyNavigationPlugin);
 
     return {
         markdownTemplateEngine: "njk",
         dir: {
             input: "src",
-            includes: "_includes",
             data: "_data",
+            includes: "_includes",
+            layouts: "_layouts",
             output: "_site",
         },
     };
