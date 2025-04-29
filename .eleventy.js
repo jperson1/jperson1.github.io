e97a44c7e71a497778634c3fc35b61f635ed055a
James Person <jperson1@umbc.edu>
2025-04-28 23:11:13 -0400

    Tailwind preprocessor credit

--- .eleventy.js
+++ .eleventy.js
@@ -12,7 +12,17 @@ import eleventyNavigationPlugin from "@11ty/eleventy-navigation";
 dotenv.config();
 
 export default (config) => {
+    // Source for Tailwindcss preprocessor: https://www.humankode.com/eleventy/how-to-set-up-tailwind-4-with-eleventy-3/
     //compile tailwind before eleventy processes the files
+    const processor = postcss([
+        //compile tailwind
+        tailwindcss(),
+
+        //minify tailwind css
+        cssnano({
+        preset: 'default',
+        }),
+    ]);
     config.on('eleventy.before', async () => {
         const tailwindInputPath = path.resolve('./src/assets/css/index.css');
 
@@ -32,16 +42,6 @@ export default (config) => {
 
         fs.writeFileSync(tailwindOutputPath, result.css);
     });
-
-    const processor = postcss([
-        //compile tailwind
-        tailwindcss(),
-
-        //minify tailwind css
-        cssnano({
-        preset: 'default',
-        }),
-    ]);
 
     config.addPassthroughCopy("src/assets/img");
     config.addPassthroughCopy("src/assets/files");
